a71043b38c5addcc4135047337c09fab044001fd
Peter <peterrsamir@gmail.com>
2024-09-01 13:53:45 +0300

    add print String file into framework

---
+++ PodsPublish/PodsPublish/PodsPublish.h
@@ -0,0 +1,18 @@
+//
+//  PodsPublish.h
+//  PodsPublish
+//
+//  Created by Peter Samir on 01/09/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PodsPublish.
+FOUNDATION_EXPORT double PodsPublishVersionNumber;
+
+//! Project version string for PodsPublish.
+FOUNDATION_EXPORT const unsigned char PodsPublishVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodsPublish/PublicHeader.h>
+
+
---
+++ PodsPublish/PodsPublish/PrintString.swift
@@ -0,0 +1,15 @@
+//
+//  PrintString.swift
+//  PodsPublish
+//
+//  Created by Peter Samir on 01/09/2024.
+//
+
+import Foundation
+
+public class PrintString {
+    
+    func PrintString() {
+        print("hello from framework....")
+    }
+}
